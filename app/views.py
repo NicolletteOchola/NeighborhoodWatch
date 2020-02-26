309bcd658279a384fdeb713cc6d955527d3c076c
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-26 14:01:14 +0300

    Added the forms for he models created

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http  import HttpResponse,Http404
+from django.shortcuts import render,redirect
+from .models import Neighborhood,Profile,Business,Post
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import ProfileForm, NewBusinessForm, NewPostForm
 
 # Create your views here.
+

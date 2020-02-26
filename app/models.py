c8a4e0602827452186db4b88e8a8abbab923e01a
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-26 13:34:35 +0300

    Added the phone number field package and other dependancies

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from pyuploadcare.dj.models import ImageField
+
 
 # Create your models here.
